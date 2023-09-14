b108530a4285ce26f0c3d7f0c2707735ca596c52
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-14 11:31:22 -0400

    updating lab

--- server/app.py
+++ server/app.py
@@ -87,14 +87,10 @@ class CheckSession(Resource):
 @app.before_request
 def check_if_logged_in():
     if not session.get('user_id') \
+        and request.endpoint in ['member_article','member_index'] \
         and request.endpoint != 'login':
         return {'error': 'Unauthorized'}, 401
-    elif session.get('user_id') \
-        and request.endpoint != 'logout' \
-        and  request.endpoint != 'member_index'\
-        and request.endpoint != 'member_article':
-        return {'error': 'Unauthorized'}, 401
-
+    
 
 class MemberOnlyIndex(Resource):
     
